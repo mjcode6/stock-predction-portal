def44bec67b36a0af8fa9084afff81cac0ae9666
sivasuthan manojan <mjcode6@gmail.com>
2025-07-11 10:13:17 +0200

    this is going to take lot of time and finally i work on how to use link to navigate between diffrent components

---
+++ frontend-react/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+    return (
+        <div>
+            <h2 className="text-light">Log In</h2>
+            {/* Log In form goes here */}
+        </div>
+    );
+}
+
+export default Login;
