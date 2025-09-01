87177bb4d61affc5ba6f845df39bb0170481fcfd
sivasuthan manojan <mjcode6@gmail.com>
2025-09-01 13:34:42 +0200

    after few days i retake this project so far it's been really good nad i work on register functionnality and rendering data to the back end but still not finish beacuse i have to install axios to make fully functionnal so i can deal with that tomorrow that's it for the day

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,8 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Register = () => {
- return (
-    <div className="text-light">Register</div>
+   const[username, setUsername] = useState("");
+   const[email, setEmail] = useState("");
+   const[password, setPassword] = useState("");
+
+   const HandleRegistration = (e) => {
+      e.preventDefault();
+     // console.log("Test");
+
+      const userData = {
+         username,
+         email,
+         password
+      };
+      console.log(userData);
+   }
+   return (
+    <>
+      <h1>Register Page</h1>
+         <div className="container">
+           <div className="row justify-content-center">
+             <div className="col-md-6 bg-light-dark p-5 rounded">
+               <h3 className="text-light text-center">Create an account</h3>
+               <form onSubmit={HandleRegistration}>
+                 <div className="mb-3">
+                   <label htmlFor="username" className="form-label text-light">Username</label>
+                   <input type="text" className="form-control" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                 </div>
+                 <div className="mb-3">
+                   <label htmlFor="email" className="form-label text-light">Email</label>
+                   <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                 </div>
+                 <div className="mb-3">
+                   <label htmlFor="password" className="form-label text-light">Password</label>
+                   <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                 </div>
+                 <button type="submit" className="btn btn-primary">Register</button>
+               </form>
+             </div>
+           </div>
+         </div>
+    </>
  )
 }
 
